9a148813b2f914e8b8d673ec7cb2a7c6b603ac5a
Alberto34315 <ruizserranoalberto5@gmail.com>
2022-04-07 08:34:49 +0200

    first commit

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -4,19 +4,29 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AgregarComponent implements OnInit {
+  texto1: string = 'Fernando Herrera';
+  color: string = 'green';
   miFormulario: FormGroup = this.fb.group({
-    nombre: ["", [Validators.required]]
-  })
-  constructor(private fb: FormBuilder) { }
+    nombre: ['', [Validators.required]],
+  });
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   tieneError(campo: string): boolean {
-    return this.miFormulario.get(campo)?.invalid || false
+    return this.miFormulario.get(campo)?.invalid || false;
+  }
+  cambiarNombre() {
+    this.texto1 = Math.random().toString();
+  }
+
+  cambiarColor() {
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    this.color = color;
   }
 }
